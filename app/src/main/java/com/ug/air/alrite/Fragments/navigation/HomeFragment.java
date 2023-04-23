77c9fa791d928ae0073a795c99f52b5d30746092
yashv3 <yashv3@uw.edu>
2023-04-23 02:25:49 -0700

    in progress - add patient from ALRITE Android App to OpenMRS (basic)

--- app/src/main/java/com/ug/air/alrite/Fragments/navigation/HomeFragment.java
+++ app/src/main/java/com/ug/air/alrite/Fragments/navigation/HomeFragment.java
@@ -1,73 +1,34 @@
 package com.ug.air.alrite.Fragments.navigation;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
-
-import static com.ug.air.alrite.Activities.SplashActivity.APP_OPENING_COUNT;
-import static com.ug.air.alrite.Activities.SplashActivity.COUNTING_DATA;
-
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.work.Constraints;
 import androidx.work.NetworkType;
-import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.ug.air.alrite.APIs.ApiClient;
 import com.ug.air.alrite.APIs.JsonPlaceHolder;
 import com.ug.air.alrite.Activities.AddPatientActivity;
-import com.ug.air.alrite.Activities.Dashboard;
 import com.ug.air.alrite.Activities.LearnActivity;
 import com.ug.air.alrite.Activities.PatientActivity;
-import com.ug.air.alrite.Activities.SplashActivity;
+import com.ug.air.alrite.Activities.PersonActivity;
 import com.ug.air.alrite.Activities.SyncActivity;
-import com.ug.air.alrite.BuildConfig;
 import com.ug.air.alrite.Database.DatabaseHelper;
-import com.ug.air.alrite.Fragments.Patient.Nasal;
-import com.ug.air.alrite.Fragments.Patient.Wheezing;
 import com.ug.air.alrite.R;
-import com.ug.air.alrite.Utils.Counter;
 import com.ug.air.alrite.Utils.Credentials;
-import com.ug.air.alrite.Worker.NotifyWorker;
 import com.ug.air.alrite.Worker.NotifyWorker2;
 import com.ug.air.alrite.Worker.NotifyWorker3;
 import com.ug.air.alrite.Worker.NotifyWorker4;
 
 import java.io.File;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 
 public class HomeFragment extends Fragment {
@@ -162,6 +123,17 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), AddPatientActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        view.findViewById(R.id.addPerson).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent(getActivity(), PersonActivity.class);
+                bundle.putInt("Fragment", 3);
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
