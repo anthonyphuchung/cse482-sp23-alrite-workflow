5a6b9c3b51a60fe1d1689c42253f69f8af1831da
Alex <76153641+alex-banzhancha@users.noreply.github.com>
2023-04-26 15:02:32 -0700

    check all

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_org_openmrs_mobile_application_OpenMRS.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "org.openmrs.mobile.application.OpenMRS"
-)
-public final class _org_openmrs_mobile_application_OpenMRS {
-}
